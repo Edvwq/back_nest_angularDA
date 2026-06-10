913249a3e3bc09f6ea66426648a2ed8e05138b2c
jryugsi21 <jry.yugsi@yavirac.edu.ec>
2026-06-10 13:31:55 -0500

    subida

--- src/modules/auth/dto/register-auth.dto.ts
+++ src/modules/auth/dto/register-auth.dto.ts
@@ -6,8 +6,9 @@ export class LoginAuthDto implements User {
     @IsNotEmpty()
     @IsEmail()
     email!: string;
+    @MinLength(6)
+    @MaxLength(25)
     @IsNotEmpty()
-    @IsEmail()
     password!: string;
 
 
